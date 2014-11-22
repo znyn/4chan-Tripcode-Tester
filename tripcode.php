4d850899df7d7a7e658c7adce00266f9eb0cc336
8BytesTooShort <kinghelian@gmail.com>
2014-11-22 15:43:15 -0800

    Same as below

--- tripcode.php
+++ tripcode.php
@@ -11,7 +11,7 @@
 // Generates a random number for the post number between 11,000 and 99,000
 $postnumber = rand(11111,999999);
 $min = rand(10,59);
-$hour = rand(10,24);
+$hour = rand(10,23);
 function tripcode($plain)
 {
     $salt = substr($plain."H.",1,2);
